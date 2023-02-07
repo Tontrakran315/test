bbbf06bc9925fa08536136e7dd62a39bd890d5dd
Tontrakran Wongstan <kimza617@gmail.com>
2023-02-07 17:08:39 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="LoadUser()">Load</button>
+  <ul>
+    <li v-for="data,i in Data" :key="i">
+      {{ data.id }} <br> {{ data.brand }} <br> {{ data.name }}
+    </li>
+  </ul>
+</template>
+<script setup lang="ts">
+import axios from"axios";
+import { ref } from "vue";
+const Data = ref([]);
+const url = ref('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+function LoadUser() {
+    axios
+    .get(url.value)
+    .then((Response)=>{
+        Data.value = Response.data;
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+
+</script>
+
+<style>
+ul{
+  margin-left: -40px;
+
+}
+ul li{
+  border: 1px solid rgb(151, 53, 53);
+  list-style: none;
+
+}
+</style>
